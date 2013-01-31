dfab6778b29a5c004aa344fe70bdb9f9fbef40d1
howete <thowe@hp.com>
2013-01-31 14:29:21 -0700

    clean up loggin

--- merge.sh
+++ merge.sh
@@ -11,18 +11,22 @@ export TERM=xterm-256color
 source "$HOME/.rvm/scripts/rvm"
 rvm use ruby-1.9.2@unix_cli
 
-set -x
+echo '============================api-site============================'
 rm -rf api-site
 git clone git@git.hpcloud.net:DevExDocs/api-site.git
 cd api-site
+set -x
 git checkout -b ${BRANCH} || git checkout ${BRANCH}
 git pull origin ${BRANCH}
 git subtree pull -P content/api git@git.hpcloud.net:DevExDocs/apihome.git ${BRANCH}
 git push origin ${BRANCH}
+set +x
 
+echo '==========================docs.hpcloud.com======================'
 rm -rf docs.hpcloud.com
 git clone git@git.hpcloud.net:DevExDocs/docs.hpcloud.com.git
 cd docs.hpcloud.com
+set -x
 git checkout -b ${BRANCH} || git checkout ${BRANCH}
 git pull origin ${BRANCH}
 git subtree pull -P content/api git@git.hpcloud.net:DevExDocs/apihome.git ${BRANCH}
