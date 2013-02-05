1a24322f43ec9194d3d360a37ddad02939857590
howete <thowe@hp.com>
2013-02-05 15:56:39 -0700

    apidocs-92 one last deploy change

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,13 @@ stackato map docs-two docs.${URL} || true
 stackato unmap docs-one docs.${URL} || true
 cp stackato-one.yml stackato.yml
 stackato update -n --nostart docs-one
+echo "Deploying http://docs-one${DOMAIN}"
 stackato start docs-one
 stackato map docs-one docs.${URL}
 stackato unmap docs-two docs.${URL} || true
 cp stackato-two.yml stackato.yml
 stackato update -n --nostart docs-two
+echo "Deploying http://docs-two.${DOMAIN}"
 stackato start docs-two
 stackato map docs-two docs.${URL}
 rm -f stackato.yml
