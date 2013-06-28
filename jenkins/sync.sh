d2d03d431d2dfe68aa5a5f9f6bf4aced85408b0a
howete <thowe@hp.com>
2013-06-28 14:30:48 -0600

    docs-140 possible move to cdn

---
+++ jenkins/sync.sh
@@ -0,0 +1,39 @@
+SRCDIR=spec/fixtures/files/
+TAINER=":tainer"
+TMPDSTFILES=/tmp/sinkdst$$
+TMPSRCFILES=/tmp/sinksrc$$
+
+cd "${SRCDIR}"
+find * | while read FILE
+do
+  if [ -f "${FILE}" ]
+  then
+    SUM=$(md5sum <"${FILE}" | sed -e 's/  *-//')
+    echo "${FILE};${SUM}"
+  fi
+done | sort >${TMPSRCFILES}
+hpcloud list "${TAINER}" -l -c sname,etag -d ';' |sort >${TMPDSTFILES}
+
+IFS=';'
+while read FILE SRCMD5SUM
+do
+  SRC="${FILE}"
+  DEST="${TAINER}/${FILE}"
+  if ! grep "^${FILE};${SRCMD5SUM}$" ${TMPDSTFILES} >/dev/null
+  then
+    hpcloud copy "${FILE}" "${DEST}"
+  fi
+done <${TMPSRCFILES}
+
+while read FILE DESTMD5SUM
+do
+  SRC="${FILE}"
+  DEST="${TAINER}/${FILE}"
+  if [ ! -f "${SRC}" ]
+  then
+    hpcloud rm "${DEST}"
+  fi
+done <${TMPDSTFILES}
+
+rm -f "${TMPDSTFILES}"
+rm -f "${TMPSRCFILES}"
