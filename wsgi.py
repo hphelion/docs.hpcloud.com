32f509630a9455eddcb4a1c68bd79355cef86b37
Matt Farina <matt@mattfarina.com>
2013-09-25 09:44:15 -0400

    MRKT-359: Removing the lookup.
    The files have been moved from swift/cdn to this git repo meaning
    the lookup and redirect is no longer needed. It has been removed.
    
    The last 3 items in it were lost and/or outdated versions. They
    were no longer needed.

--- wsgi.py
+++ wsgi.py
@@ -1,26 +1,13 @@
 import os
 from wsgiref.simple_server import make_server
 
-# Lookup is a lookup for redirects to files in object storage.
-lookup = {}
-lookup['/file/hpNET-1.3.0.6.nupkg'] = 'https://a248.e.akamai.net/cdn.hpcloudsvc.com/hdb0a98359ca04396dd429335015a1b4b/prodaw2/HPCloud-API.1.3.0.6.nupkg'
-lookup['/file/hpPHP-1.1.0.zip'] = 'https://a248.e.akamai.net/cdn.hpcloudsvc.com/hdb0a98359ca04396dd429335015a1b4b/prodaw2/HPCloud-PHP-1.1.0.zip'
-lookup['/file/nova-stuff.tar'] = 'https://a248.e.akamai.net/cdn.hpcloudsvc.com/hdb0a98359ca04396dd429335015a1b4b/prodaw2/prettytable-0.5.tar.gz'
-
-
 def handle_no_file(env, start_response):
-    if env['PATH_INFO'] in lookup:
-        status = '307 Temporary Redirect'  # HTTP temporary redirect.
-        headers = [('Location', lookup[env['PATH_INFO']])]
-        start_response(status, headers)
-        return env['PATH_INFO']
-    else:
-        f = open('_site/404error.html')
-        status = '404 Not Found'  # HTTP Status
-        headers = [('Content-type', 'text/html')]  # HTTP Headers
-        start_response(status, headers)
+    f = open('_site/404error.html')
+    status = '404 Not Found'  # HTTP Status
+    headers = [('Content-type', 'text/html')]  # HTTP Headers
+    start_response(status, headers)
 
-        return f.readlines()
+    return f.readlines()
 
 application = handle_no_file
 
