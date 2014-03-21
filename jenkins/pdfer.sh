b8a40296ca40861ea04ff591573474e235a9fe33
Terry Howe <terrylhowe@gmail.com>
2014-03-21 17:11:04 -0600

    add pdf script

---
+++ jenkins/pdfer.sh
@@ -0,0 +1,13 @@
+cd _site
+find cloudos -name index.html | 
+while read FILE
+do
+   DIR=$(echo $FILE | sed -e 's/index.html//')
+   RELATIVE=$(echo $DIR| sed -e 's,[^/]*/,\.\./,g')
+   PDFFILE=$(echo $FILE | sed -e 's/index.html/index.pdf/' -e 's,cloudos,pdf/cloudos,')
+   echo "============== $DIR - $RELATIVE"
+   sed -i -e "s,/cloudos/\([^\"]*\)\",${RELATIVE}cloudos/\1index.pdf\",g" $FILE
+   mkdir -p $DIR
+   wkhtmltopdf $FILE $PDFFILE
+   echo "============== $PDFFILE"
+done
