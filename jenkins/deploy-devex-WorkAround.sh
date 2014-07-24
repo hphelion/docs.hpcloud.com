6941b6907ccd8afc24ed17384605aecea56785ba
Greg Larsen <greg.larsen@hp.com>
2014-07-24 09:41:29 +0200

    first commit

---
+++ jenkins/deploy-devex-WorkAround.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+#
+# Generic deploy script
+#
+
+echo 'running docs.hpcloud.com/jenkins/deploy.sh'
+if [ -z "${1}" -o -z "${2}" ]
+then
+  echo "Usage: ${0} application domain <url>"
+  exit 1
+fi
+APP="${1}"
+DOMAIN="${2}"
+if [ -n "${3}" ]
+then
+  URL=${3}
+else
+  URL=${DOMAIN}
+fi
+echo "Deploying http://docs.${URL}"
+set -x
+/usr/bin/stackato target https://api.${DOMAIN}
+/usr/bin/stackato login
+/usr/bin/stackato group Documentation
+/usr/bin/stackato unmap docs-${APP} docs.${URL} || true
+cp stackato-${APP}.yml stackato.yml
+/usr/bin/stackato stop docs-${APP}
+#stackato update -n --nostart docs-${APP} ||
+#stackato update -n --nostart docs-${APP}
+/usr/bin/stackato push docs-${APP} ||
+/usr/bin/stackato push docs-${APP}
+echo "Deploying http://docs-${APP}.${DOMAIN}"
+/usr/bin/stackato start docs-${APP}
+/usr/bin/stackato map docs-${APP} docs.${URL}
+rm -f stackato.yml
+echo "Done."
+exit 0
