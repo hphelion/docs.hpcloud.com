5b2573cb71989ec6e487c697a215b3a794417fc7
Greg Larsen <greg.larsen@hp.com>
2014-08-06 16:22:59 +0200

    initial commit

---
+++ jenkins/build.development.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+#
+# Run a deploy to qa
+#
+
+echo 'running docs.hpcloud.com/jenkins/deploy-qa.sh'
+export TERM=xterm-256color
+source "$HOME/.rvm/scripts/rvm"
+rvm use ruby-1.9.2-p320@docs
+
+echo '=debug==============================================================='
+echo "LOGIN IS: ${LOGIN}"
+echo "PASSWORD IS: ${PASSWORD}"
+
+
+echo '=git================================================================='
+git checkout -f development
+git pull origin development
+
+echo '=content============================================================='
+./jenkins/api.sh develop
+./jenkins/documentation.sh development
+
+echo '=build==============================================================='
+./jenkins/clean.sh
+./jenkins/build.sh
+
+
+./jenkins/server
+ 
