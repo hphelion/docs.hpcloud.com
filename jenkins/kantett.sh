a6d8352b3bbaec914dd8ab6ffa95412329efc7aa
Greg Larsen <greg.larsen@hp.com>
2014-09-28 17:17:30 +0200

    script to ftp to test server

--- jenkins/kantett.sh
+++ jenkins/kantett.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
  
-cd /home/jenkins/DevExBuild/workspace/docs-emergency-build/_site/
- 
+remote=/
+folder=$1
+
+cd /home/jenkins/DevExBuild/workspace/docs-emergency-build/_site/$folder
+
 ftp -np kantett.de <<SCRIPT
 user w007a881 only4kantett
 ascii
-put index.html
-quit
+mkdir $remote/$folder
+cd $remote/$folder
+mput *
+close
+bye
 SCRIPT
 exit 0
+
+
+ 
+
