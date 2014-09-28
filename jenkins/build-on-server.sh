b60832d0ee35ca1710c8c2bd86b6c83ddba461d2
Greg Larsen <greg.larsen@hp.com>
2014-09-28 15:21:53 +0200

    revert

--- jenkins/build-on-server.sh
+++ jenkins/build-on-server.sh
@@ -32,9 +32,9 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 
-ejekyll build --trace
+ejekyll serve --detach
 
-cp _site/* /www/var/
+ 
 
 #
 # Special post-build logo handling for Cloud OS docs:
