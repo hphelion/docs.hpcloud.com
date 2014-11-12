6fc27ab805c11b458c3bc652eef7eb2ffe14bc99
Greg Larsen <greg.larsen@hp.com>
2014-11-12 14:01:55 +0100

    remove special post-build steps for cloudos moonshot

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -18,8 +18,8 @@ ejekyll build --trace
 #
 # Next, replace Sign Up Now text (which is irrelevant for Cloud OS private clouds) and link target with For More Info and better Cloud OS target URL:
 #
-find _site/cloudos -name "index.html" -print | xargs sed -i -e 's#<a href="https://console.hpcloud.com/signup" class="signup-button-orange gutter-top-half">Sign Up Now</a>#<a href="https://cloudos.hpwsportal.com" target="_blank" class="signup-button-orange gutter-top-half">For More Info</a>#g'
+#find _site/cloudos -name "index.html" -print | xargs sed -i -e 's#<a href="https://console.hpcloud.com/signup" class="signup-button-orange gutter-top-half">Sign Up Now</a>#<a href="https://cloudos.hpwsportal.com" target="_blank" class="signup-button-orange gutter-top-half">For More Info</a>#g'
 #
 # Now replace For More Info target link on Cloud OS for Moonshot pages:
 #
-find _site/cloudos/moonshot -name "index.html" -print | xargs sed -i -e 's#<a href="https://cloudos.hpwsportal.com" target="_blank" class="signup-button-orange gutter-top-half">For More Info</a>#<a href="http://www.hp.com/go/moonshot" target="_blank" class="signup-button-orange gutter-top-half">For More Info</a>#g'
+#find _site/cloudos/moonshot -name "index.html" -print | xargs sed -i -e 's#<a href="https://cloudos.hpwsportal.com" target="_blank" class="signup-button-orange gutter-top-half">For More Info</a>#<a href="http://www.hp.com/go/moonshot" target="_blank" class="signup-button-orange gutter-top-half">For More Info</a>#g'
