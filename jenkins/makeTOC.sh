dc4939a8cac70ed5f72aa4da2f96f63121922fd4
Greg Larsen <greg.larsen@hp.com>
2015-03-27 17:09:36 +0100

    change name

--- jenkins/makeTOC.sh
+++ jenkins/makeTOC.sh
@@ -2,20 +2,34 @@
  
 
 #Delete any tempfiles left over from the last run and write introduction
-rm 1.1master-toc.md > /dev/null 2>&1
-rm 1.1master-toc.tmp > /dev/null 2>&1
+rm master-toc.md > /dev/null 2>&1
+rm master-toc.tmp > /dev/null 2>&1
  
-echo "---" > 1.1master-toc.md
-echo "layout: defaul"t >> 1.1master-toc.md
-echo "title: \"Master TOC\"" >> 1.1master-toc.md
-echo "permalink: /1.1/contents/" >> 1.1master-toc.md
-echo "---" >> 1.1master-toc.md
-echo "<!--UNDER REVISION-->" >> 1.1master-toc.md
-echo "#Master TOC#" >> 1.1master-toc.md
-echo "This file is rebuilt each time docs-emergency-build is run."  >> 1.1master-toc.md
-echo "The file is not copied to the master branch and not pushed to production." >> 1.1master-toc.md
-echo " " >> 1.1master-toc.md
-  
+echo "---" > master-toc.md
+echo "layout: defaul"t >> master-toc.md
+echo "title: \"Master TOC\"" >> master-toc.md
+echo "permalink: /master-toc/" >> master-toc.md
+echo "---" >> master-toc.md
+echo "<!--UNDER REVISION-->" >> master-toc.md
+echo "#Master TOC#" >> master-toc.md
+echo "This file is rebuilt each time docs-emergency-build is run."  >> master-toc.md
+echo "The file is not copied to the master branch and not pushed to production." >> master-toc.md
+echo " " >> master-toc.md
+
+echo "<script>
+(function() {
+var cx = '016914425578488494023:saefz1ifgsc';
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+'//www.google.com/cse/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+ s.parentNode.insertBefore(gcse, s);
+})();
+</script>
+<gcse:search></gcse:search>" >> master-toc.md 
+ 
 for i in `find . -path ./content/documentation/redirects -prune -o -name "*.md" -print`
  
 do
@@ -25,11 +39,11 @@ do
 	then		
 if [[ -z $(grep  redirect $i) ]]
 then
-echo "* ["`head -10 $i | grep title | sed 's/title: //'| sed 's/"//g'`"]("`head $i | grep permalink | sed 's/permalink: //'`")" >> 1.1master-toc.tmp
+echo "* ["`head -10 $i | grep title | sed 's/title: //'| sed 's/"//g'`"]("`head $i | grep permalink | sed 's/permalink: //'`")" >> master-toc.tmp
 fi
 	fi
 
 
 done
 
-sort -t "(" -k 2 1.1master-toc.tmp | grep -v "\[\]\(\)"  | grep -v "error.html" | grep -v "1.0"  | grep -v "1.01"  >> 1.1master-toc.md
+sort -t "(" -k 2 master-toc.tmp | grep -v "\[\]\(\)"  | grep -v "error.html" | grep -v "1.0"  | grep -v "1.01"  >> master-toc.md
