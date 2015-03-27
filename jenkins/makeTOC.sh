d2609154e40da9d84013f4a394f04d68f6e8aa3e
Greg Larsen <greg.larsen@hp.com>
2015-03-27 17:30:02 +0100

    change name

--- jenkins/makeTOC.sh
+++ jenkins/makeTOC.sh
@@ -12,26 +12,12 @@ echo "permalink: /master-toc/" >> master-toc.md
 echo "---" >> master-toc.md
 echo "<!--UNDER REVISION-->" >> master-toc.md
 echo "#Master TOC#" >> master-toc.md
-echo "This file is rebuilt each time docs-emergency-build is run."  >> master-toc.md
-echo "The file is not copied to the master branch and not pushed to production." >> master-toc.md
+echo "Below is a list of all the pages on docs.hpcloud.com"  >> master-toc.md
 echo " " >> master-toc.md
-
-echo "
-<div>
-<script>
-(function() {
-var cx = '016914425578488494023:saefz1ifgsc';
-var gcse = document.createElement('script');
-gcse.type = 'text/javascript';
-gcse.async = true;
-gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-'//www.google.com/cse/cse.js?cx=' + cx;
-var s = document.getElementsByTagName('script')[0];
- s.parentNode.insertBefore(gcse, s);
-})();
-</script>
-<gcse:search></gcse:search> 
-</div>" >> master-toc.md 
+echo " " >> master-toc.md
+ 
+ 
+ 
  
 for i in `find . -path ./content/documentation/redirects -prune -o -name "*.md" -print`
  
