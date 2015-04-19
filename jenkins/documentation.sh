802e4dd6cef14730a5b01c3da61cc44a79fcc43e
Greg Larsen <greg.larsen@hp.com>
2015-04-19 17:54:16 +0200

    merge from master

--- jenkins/documentation.sh
+++ jenkins/documentation.sh
@@ -13,7 +13,8 @@ BRANCH="${1}"
 mkdir -p content
 cd content
 rm -rf documentation
-git clone git@git.hpcloud.net:DevExDocs/documentation.git
+#git clone git@github.com:hphelion/documentation.git
+git clone https://github.com/hphelion/documentation.git
 cd documentation
 git checkout "${BRANCH}"
 git pull origin "${BRANCH}"
