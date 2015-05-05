cf9acf31cd6d39be532b383ee3b0de86a798955e
Greg Larsen <greg.larsen@hp.com>
2015-05-05 16:27:48 +0200

    change branch of documentation to helion1.1

--- jenkins/deploy-pro.sh
+++ jenkins/deploy-pro.sh
@@ -14,7 +14,7 @@ git pull origin master
 
 echo '=content============================================================='
 
-./jenkins/documentation.sh master
+./jenkins/documentation.sh helion1.1
 ./jenkins/api.sh 
 
 echo '=build==============================================================='
